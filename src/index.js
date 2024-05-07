a1bdf8d00fd467ad523f9269c2637aefa5545465
ayesha <ayeshaakh234@gmail.com>
2024-05-07 17:39:23 +0500

    Normalize line endings

---
+++ src/index.js
@@ -0,0 +1,44 @@
+//require('dotenv').config({path:'./env'})
+import dotenv from "dotenv"
+
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:'./env'
+})
+
+connectDB()
+.then(()=>{
+  app.listen(process.env.PORT||8000),()=>{
+    console.log(`server running on port${process.env.PORT}`);
+  }
+})
+.catch((err)=>{
+  console.log("mogodb ConnectionFailed");
+})
+/*
+import express from "express"
+
+const app = "express"
+;(async()=>{
+    try{
+
+      await mongoose.connect(`${process.env.MONGODB_URI}${DB_NAME}`)
+      app.on("error",(error)=>{
+        console.log("ERRR:",error);
+        throw error
+      })
+      app.listen(process.env.PORT,()=>{
+        console.log(`app is listening on port ${
+            process.env.PORT
+        }`);
+      }
+        
+      )
+    }
+    catch(error){
+        console.error("error:",error);
+
+        throw err
+    }
+})()*/
